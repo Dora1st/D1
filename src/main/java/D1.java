4cd62d1923cb5702af7d75cd901cef8231034c72
Дарья <cold.dashytka@yandex.ru>
2023-03-14 17:54:44 +0100

    Tast

---
+++ src/main/java/D1.java
@@ -0,0 +1,31 @@
+public class D1 {
+    public static void main(String[] args) {
+        System.out.println("sun");
+        int q = 12;
+        int w;
+        w = 13;
+        System.out.println(w);
+        w = 15;
+        System.out.println(w);
+        w = w + w;
+        System.out.println(w);
+        String a = "p;olikuj";
+        int e;
+        e = q;
+        double y = 4d/3;
+        System.out.println(y);
+        boolean u = true;
+        boolean i = !u;
+        if (3>4) {
+            System.out.println("lkjhgfdsiuytfdsa");
+        } else {
+            System.out.println("e0r987654");
+        }
+        boolean b = (3 > 0) || (4 >= 4);
+        boolean b = (3 < 0) && (4 >= 4);
+        boolean yyy = 4 >=4;
+        System.out.println(yyy);
+
+
+    }
+}
